4e87db5ddd54d4fb62be40f10b1c8ef706980029
maryam13188 <roro13188@gmail.com>
2026-01-15 06:43:52 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
-# Import the add function from add_0.py
 from add_0 import add
 
-# Define variables a and b in separate lines
 a = 1
 b = 2
 
-# Calculate the result using the imported function
 result = add(a, b)
 
-# Print the formatted result
 print("{:d} + {:d} = {:d}".format(a, b, result))
 
-# Prevent execution when imported
 if __name__ == "__main__":
     pass
