d257918b6fee2bce8404e6e18650fa42dd634691
maryam13188 <roro13188@gmail.com>
2026-01-18 08:17:59 +0000

    fix: Modify list in-place for Task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@
 def delete_at(my_list=[], idx=0):
     """Delete item at specific position."""
     if 0 <= idx < len(my_list):
-        return my_list[:idx] + my_list[idx+1:]
+        del my_list[idx]
     return my_list
