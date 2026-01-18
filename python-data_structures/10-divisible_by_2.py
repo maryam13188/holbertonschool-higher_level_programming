6f43c6f94a712482e7ed483acfdcbb2446e86895
maryam13188 <roro13188@gmail.com>
2026-01-18 08:05:18 +0000

    fix: Ensure correct function implementation

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2_alt3(my_list=[]):
-    """Using while loop."""
-    result = []
-    i = 0
-    while i < len(my_list):
-        result.append(my_list[i] % 2 == 0)
-        i += 1
-    return result
+
+def divisible_by_2(my_list=[]):
+    """Return list of True/False for numbers divisible by 2."""
+    return [x % 2 == 0 for x in my_list]
