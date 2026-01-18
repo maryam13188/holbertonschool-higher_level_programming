9c1b54988fe3a9ea100ffeb24e408c5be0a8a069
maryam13188 <roro13188@gmail.com>
2026-01-18 04:16:29 +0000

    feat: Add Task 3 - Print list in reverse order

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order."""
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
+
+
+# Test function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_reversed_list_integer(my_list)
