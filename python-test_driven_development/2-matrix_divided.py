7d5a17bf07829754f0ac6bc5aab5f275477aebef
maryam13188 <roro13188@gmail.com>
2026-01-20 12:09:45 +0000

    add

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,43 +6,35 @@ def matrix_divided(matrix, div):
     """Divide matrix elements by div"""
     err_msg = "matrix must be a matrix (list of lists) of integers/floats"
     
-    # Check matrix is valid
     if not isinstance(matrix, list) or not matrix:
         raise TypeError(err_msg)
     
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError(err_msg)
+    for row in matrix:
+        if not isinstance(row, list) or not row:
+            raise TypeError(err_msg)
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(err_msg)
     
-    if not all(row for row in matrix):
-        raise TypeError(err_msg)
+    size = len(matrix[0])
+    for row in matrix:
+        if len(row) != size:
+            raise TypeError("Each row of the matrix must have the same size")
     
-    # Check all elements are numbers
-    if not all(isinstance(num, (int, float))
-               for row in matrix for num in row):
-        raise TypeError(err_msg)
-    
-    # Check all rows have same size
-    first_len = len(matrix[0])
-    if not all(len(row) == first_len for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    
-    # Check div is valid
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     
     if div == 0:
         raise ZeroDivisionError("division by zero")
     
-    # Perform division and rounding
-    result_matrix = []
+    new_matrix = []
     for row in matrix:
         new_row = []
         for num in row:
-            rounded = round(num / div, 2)
-            # Handle -0.0 case
-            if rounded == -0.0:
-                rounded = 0.0
-            new_row.append(rounded)
-        result_matrix.append(new_row)
+            result = round(num / div, 2)
+            if result == -0.0:
+                result = 0.0
+            new_row.append(result)
+        new_matrix.append(new_row)
     
-    return result_matrix
+    return new_matrix
