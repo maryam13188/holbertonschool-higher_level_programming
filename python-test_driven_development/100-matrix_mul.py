4db06f677a70b6320b27292164ed3b638c397407
maryam13188 <roro13188@gmail.com>
2026-01-22 07:07:54 +0000

    add

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -27,7 +27,7 @@ def matrix_mul(m_a, m_b):
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     
-    # Validate m_b  
+    # Validate m_b
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
     if not all(isinstance(row, list) for row in m_b):
@@ -36,25 +36,19 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
     
     # Validate elements in m_a
-    row_len_a = None
+    row_len_a = len(m_a[0])
     for row in m_a:
-        if row_len_a is None:
-            row_len_a = len(row)
-        elif len(row) != row_len_a:
+        if len(row) != row_len_a:
             raise TypeError("each row of m_a must be of the same size")
-        
         for element in row:
             if not isinstance(element, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
     
     # Validate elements in m_b
-    row_len_b = None
+    row_len_b = len(m_b[0])
     for row in m_b:
-        if row_len_b is None:
-            row_len_b = len(row)
-        elif len(row) != row_len_b:
+        if len(row) != row_len_b:
             raise TypeError("each row of m_b must be of the same size")
-        
         for element in row:
             if not isinstance(element, (int, float)):
                 raise TypeError("m_b should contain only integers or floats")
