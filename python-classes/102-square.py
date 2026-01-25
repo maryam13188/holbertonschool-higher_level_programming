740ec7c5959535c68755fbf58c93086a03bb5645
maryam13188 <roro13188@gmail.com>
2026-01-25 10:10:10 +0000

    add 102-square.py

---
+++ python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Defines a Square class with comparison operators based on area."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance.
+
+        Args:
+            size (int or float): Size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+
+        Raises:
+            TypeError: If value is not a number.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
+
+    # Comparison operators based on area
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
