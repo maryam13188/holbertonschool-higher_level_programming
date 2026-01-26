5b4604ef7c67fe3db3d50e29ca67a45493546b26
maryam13188 <roro13188@gmail.com>
2026-01-26 17:32:25 +0300

    Create 10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+10-square module
+Defines a Square class that inherits from Rectangle (9-rectangle)
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class, inherits from Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): size of the square
+
+        Raises:
+            TypeError, ValueError: If size is invalid
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
