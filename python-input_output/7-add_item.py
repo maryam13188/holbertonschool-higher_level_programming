3f001474a2ce52a826ed1826d9a59d6c43b07d3a
maryam13188 <roro13188@gmail.com>
2026-02-01 21:01:14 +0300

    Create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list and saves them to a file."""
+
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
