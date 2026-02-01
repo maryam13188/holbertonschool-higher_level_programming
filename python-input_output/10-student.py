96cb5fcd586bd322cfc8fa39b8f7250f53a247b0
maryam13188 <roro13188@gmail.com>
2026-02-01 21:14:26 +0300

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,11 +4,14 @@
 
 
 class Student:
-    """Student class with first_name, last_name, age, and JSON representation.
+    """Student class with first_name, last_name, age,
+    and JSON representation.
     """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
+        """Initialize a Student instance with first_name, last_name,
+        and age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -18,5 +21,6 @@ class Student:
         If attrs is a list, only attributes in this list are included.
         """
         if isinstance(attrs, list):
-            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+            return {key: self.__dict__[key] for key in attrs
+                    if key in self.__dict__}
         return self.__dict__.copy()
