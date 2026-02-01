10ab8c5082b238796d34745c950a1aeff8f475f4
maryam13188 <roro13188@gmail.com>
2026-02-01 21:10:47 +0300

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,7 +3,9 @@
 
 
 class Student:
-    """Student class with first_name, last_name, age, and JSON representation."""
+    """Student class with first_name, last_name, age, 
+    and JSON representation.
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initialize a Student instance with first_name, last_name, and age."""
