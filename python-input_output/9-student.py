bfff524031f0dbe8e0168b755d71489764f30370
maryam13188 <roro13188@gmail.com>
2026-02-01 21:09:23 +0300

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Student class with first_name, last_name, age, and JSON representation."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of the Student instance."""
+        return self.__dict__.copy()
