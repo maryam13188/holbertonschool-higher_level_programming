226c3e00eed4ead94461f9760ea950fa9f6b3c95
maryam13188 <roro13188@gmail.com>
2026-02-22 13:49:28 +0300

    Create 4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- 4-first_table.sql
+-- This script creates a table called first_table in the current database
+-- Task 4: First table
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
