d78c0f79154df0c62fc0d6e85f648bb3c8d148d9
maryam13188 <roro13188@gmail.com>
2026-02-24 12:07:10 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,48 +1,25 @@
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_4_usa with their state names
-Results sorted in ascending order by cities.id
-Uses only one execute() statement
-"""
-
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # Create cursor
     cur = db.cursor()
-
-    # Execute JOIN query (only once)
-    query = """
+    cur.execute("""
         SELECT cities.id, cities.name, states.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
-    """
-    cur.execute(query)
-
-    # Fetch and print results
-    rows = cur.fetchall()
-    for row in rows:
+    """)
+    for row in cur.fetchall():
         print(row)
-
-    # Close cursor and database connection
     cur.close()
     db.close()
-
