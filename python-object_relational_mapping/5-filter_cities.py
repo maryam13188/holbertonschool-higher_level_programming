7453907279f0c5b3a3fb50c788347044e654d566
maryam13188 <roro13188@gmail.com>
2026-02-24 10:06:25 +0000

    Add model_state.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+    cur.execute("""
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (state_name,))
+
+    rows = cur.fetchall()
+    cities = []
+    for row in rows:
+        cities.append(row[0])
+
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
+
