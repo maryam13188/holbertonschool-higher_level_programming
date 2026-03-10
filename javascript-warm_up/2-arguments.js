072e2202a1d19f685a450a311769dea1ec7eae54
maryam13188 <roro13188@gmail.com>
2026-03-10 21:31:48 +0000

    add

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argsCount = process.argv.length - 2; // process.argv[0] = node, [1] = script name
+
+if (argsCount === 0) {
+    console.log('No argument');
+} else if (argsCount === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
