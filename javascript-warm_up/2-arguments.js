96804aa58536007a1621a2f677c5f4e662f1aa33
maryam13188 <roro13188@gmail.com>
2026-03-10 21:39:30 +0000

    Fix indentation to 2 spaces using semistandard --fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const argsCount = process.argv.length - 2; // process.argv[0] = node, [1] = script name
 
 if (argsCount === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argsCount === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
